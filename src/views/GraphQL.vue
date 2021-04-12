13f12405a8c6f1ce952ea933b4784f8d63d84730
Lee, JunHyuk <leejh10003@gmail.com>
2021-04-12 12:50:34 +0900

    Do not restart graphql in components

--- src/views/GraphQL.vue
+++ src/views/GraphQL.vue
@@ -5,13 +5,7 @@
 </template>
 <script>
 import gql from 'graphql-tag'
-//import { restartWebsockets } from 'vue-cli-plugin-apollo/graphql-client'
 export default {
-  created: function (){
-    if (this.$apollo.provider.defaultClient.wsClient){
-      //restartWebsockets(this.$apollo.provider.defaultClient.wsClient)
-    }
-  },
   apollo: {
     projects: {
       query: gql`query {
