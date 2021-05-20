b4ae70723bdaf1330ed04f32987790dbf655eac2
Lee, JunHyuk <leejh10003@gmail.com>
2021-05-20 14:28:03 +0900

    Clone

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,23 +1,48 @@
 <template>
-  <VideoEditingTimeline :config="config" />
+  <div id="app">
+    <div :style="{width: '500px', height: '100px'}">
+      <VueDragResize
+          :isActive="true"
+          :w="200"
+          :h="200"
+          :sticks="['mr', 'ml']"
+          :snapToGrid="true"
+          v-on:resizing="resize"
+          v-on:dragging="resize"
+          :parentLimitation="true"
+        >
+          <h3>Hello World!</h3>
+          <p>{{ top }} х {{ left }}</p>
+          <p>{{ width }} х {{ height }}</p>
+        </VueDragResize>
+    </div>
+  </div>
 </template>
 
 <script>
-import VideoEditingTimeline from 'video-editing-timeline-vue';
+import VueDragResize from "vue-drag-resize";
 
 export default {
   components: {
-    VideoEditingTimeline: VideoEditingTimeline
+    VueDragResize,
   },
+
   data() {
     return {
-       config: {
-            canvasWidth: 5000,
-            canvasHeight: 50,
-            minimumScale: 10, // 一个小刻度长度(单位px)
-            minimumScaleTime: 1, // 一个小刻度代表时间（单位秒）
-       }
+      width: 0,
+      height: 0,
+      top: 0,
+      left: 0,
     };
-  }
+  },
+
+  methods: {
+    resize(newRect) {
+      this.width = Math.floor(newRect.width/10) * 10;
+      this.height = Math.floor(newRect.height/10) * 10;
+      this.top = Math.floor(newRect.top/10) * 10;
+      this.left = Math.floor(newRect.left/10) * 10;
+    },
+  },
 };
 </script>
