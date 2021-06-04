9697aeb6f9f86e91d7510619847e19a8abf8c749
Lee, JunHyuk <leejh10003@gmail.com>
2021-06-04 12:10:44 +0900

    Copy secret file on build

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ mv /home/ubuntu/frontend.zip /home/ubuntu/deploy/frontend.zip
 cd /home/ubuntu/deploy
 unzip frontend.zip
 rm frontend.zip
+cp /home/ubuntu/aws-exports.js /home/ubuntu/deploy/src
 \. ~/.nvm/nvm.sh
 \. ~/.profile
 \. ~/.bashrc
